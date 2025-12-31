58438004c7a1ebf759d43a56ae5a555613a5a522
programacionparaaprender <yancel209@gmail.com>
2025-12-31 17:28:25 -0500

    agregando cambios

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
   
     plugins: [
       new ModuleFederationPlugin({
-        name: "mf_crm_clients",
+        name: "mf_crm_clients_ts",
         filename: "remoteEntry.js",
         remotes: {},
         exposes: {
